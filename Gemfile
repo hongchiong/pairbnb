99023063e12d7730cfe81f97fc8d5805957f6cbc
nextacademy <hyar001@e.ntu.edu.sg>
2016-08-01 15:46:08 +0800

    installed Figaro Gem

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 #User Authentication
 gem "clearance"
 
+gem "figaro"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  root 'welcome#index'
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
